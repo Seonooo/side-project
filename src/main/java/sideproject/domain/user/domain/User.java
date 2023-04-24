1e0747b5e02f62f65348303f9f42e4b71cee35a2
seonho <eedd22@naver.com>
2023-04-24 23:03:30 +0900

    feat: #1 유저 도메인 생성

---
+++ src/main/java/sideproject/domain/user/domain/User.java
@@ -0,0 +1,36 @@
+package sideproject.domain.user.domain;
+
+import lombok.*;
+import sideproject.global.domain.BaseTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseTime {
+
+    @Id
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+    @Column(nullable = false, length = 32)
+    private String password;
+    @Column(nullable = false, unique = true, length = 12)
+    private String nickname;
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Builder
+    public User(String email, String name, String password, String nickname, String role) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
---
+++ src/main/java/sideproject/global/domain/BaseTime.java
@@ -0,0 +1,31 @@
+package sideproject.global.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+
+    @CreatedDate
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP", updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "modified_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void getCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
