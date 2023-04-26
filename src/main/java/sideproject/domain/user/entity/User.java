6eaec6b92c389703d9bb48af62642e720ecc3d11
seonho <eedd22@naver.com>
2023-04-26 22:28:30 +0900

    refactor: User Entity 구성 변경

--- src/main/java/sideproject/domain/user/entity/User.java
+++ src/main/java/sideproject/domain/user/entity/User.java
@@ -13,20 +13,22 @@ import javax.persistence.Id;
 public class User extends BaseTime {
 
     @Id
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
+    private String id;
+    @Column(nullable = false, unique = true)
     private String email;
-
     @Column(nullable = false, length = 20)
     private String name;
     @Column(nullable = false, length = 32)
     private String password;
-    @Column(nullable = false, unique = true, length = 12)
+    @Column(nullable = false, length = 12)
     private String nickname;
     @Column(nullable = false, length = 20)
     private String role;
 
     @Builder
-    public User(String email, String name, String password, String nickname, String role) {
+    public User(String id, String email, String name, String password, String nickname, String role) {
+        this.id = id;
         this.email = email;
         this.name = name;
         this.password = password;
